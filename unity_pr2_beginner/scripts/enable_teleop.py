466090efd63aa9f68a535d07dba294c4e64730a5
Smilels <sli@informatik.uni-hamburg.de>
2022-05-13 18:17:22 +0200

    pr2_control code first version

---
+++ unity_pr2_beginner/scripts/enable_teleop.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import Int8
+import tty, sys, termios
+
+if __name__ == '__main__':
+    rospy.init_node('enable_teleop', anonymous=True)
+    pub = rospy.Publisher('enable_teleop', Int8, queue_size=1)
+
+    filedescriptors = termios.tcgetattr(sys.stdin)
+    tty.setcbreak(sys.stdin)
+
+    msg = Int8()
+    msg.data = 0
+
+    print("[0] Stop all")
+    print("[1] Start left")
+    print("[2] Start right")
+    print("[3] Stop left")
+    print("[4] Stop right")
+
+    while not rospy.is_shutdown():
+        key = sys.stdin.read(1)[0]
+        if key  == "0" or key == " ":
+            print("Stop all");
+            msg.data = 0
+        elif key  == "1":
+            print("Start left");
+            msg.data = 1
+        elif key  == "2":
+            print("Start right");
+            msg.data = 2
+        elif key  == "3":
+            print("Stop left");
+            msg.data = 3
+        elif key  == "4":
+            print("Stop right");
+            msg.data = 4
+        else:
+            continue
+        pub.publish(msg)
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN,filedescriptors)
