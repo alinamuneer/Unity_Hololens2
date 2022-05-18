ec49bc698ad9a7db6e4c1cb6b9a137e5cc636435
Alina Munir <alina.munir@studium.uni-hamburg.de>
2022-05-18 16:40:02 +0000

    Upload files to '20220429RosSharp/Assets/scripts'

---
+++ 20220429RosSharp/Assets/scripts/PosePublisher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class PosePublisher : UnityPublisher<MessageTypes.Geometry.Pose>
+    {
+        public MessageTypes.Geometry.Pose WristPose;
+        public bool publish = false;
+        private MessageTypes.Geometry.Pose message;
+
+       
+        protected override void Start()
+        {
+            base.Start();
+        }
+
+        private void FixedUpdate()
+        {
+            UpdateMessage();
+        }
+
+        private void UpdateMessage()
+        {
+            message = WristPose;
+            if (publish)
+                Publish(message);
+        }
+
+    }
+}
