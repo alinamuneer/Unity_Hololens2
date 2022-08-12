59afe28530c0eeac4daa6db194bd34dd30f28dad
pr2 <pr2@tams>
2022-08-12 10:25:32 +0000

    parallel grasp with trajectory controller
    code comments organization

--- unity_pr2_beginner/src/sr_parallel_grasp_controller.cpp
+++ unity_pr2_beginner/src/sr_parallel_grasp_controller.cpp
@@ -1,4 +1,5 @@
 #include <ros/ros.h>
+#include <trajectory_msgs/JointTrajectory.h>
 #include <std_msgs/Float32.h>
 #include <std_msgs/Float64.h>
 std::vector<std::string> joint_names{"ffj0", "ffj3", "ffj4", "mfj0", "mfj3",
@@ -9,20 +10,12 @@ std::vector<std::string> joint_names{"ffj0", "ffj3", "ffj4", "mfj0", "mfj3",
 class ParallelGraspController {
 public:
   std::vector<ros::Publisher> publisher;
-  ros::Publisher ffj3_pub;
-  ros::Publisher mfj3_pub;
-  ros::Publisher thj4_pub;
-  ros::Publisher thj5_pub;
+  ros::Publisher pub;
   ros::Subscriber sub;
 
   ParallelGraspController() {
     ros::NodeHandle nh;
-
-    for (auto &joint : joint_names) {
-      publisher.push_back(nh.advertise<std_msgs::Float64>(
-          "sh_rh_" + joint + "_position_controller/command", 1));
-    }
-
+    pub = nh.advertise<trajectory_msgs::JointTrajectory>("rh_trajectory_controller/command",1);
     sub = nh.subscribe("/rh_parallel_grasp_controller/command", 1,
                        &ParallelGraspController::callback, this);
   }
@@ -30,21 +23,33 @@ public:
   void callback(const std_msgs::Float32::ConstPtr &msg) {
     // value should be opening state in cm, this will probably not be the case
     // value will be between 0.0 and 0.15
-    std_msgs::Float64 f;
+    // std_msgs::Float64 f;
+
+    trajectory_msgs::JointTrajectory arm_joint_trajectory;
+    arm_joint_trajectory.points.emplace_back();
 
     for (int i = 0; i < joint_names.size(); i++) {
-      if (joint_names[i] == "ffj3")
-        f.data = 1.47 - msg->data * 10;
-      else if (joint_names[i] == "mfj3")
-        f.data = 1.57 - msg->data * 10;
-      else if (joint_names[i] == "thj4")
-        f.data = 1.20;
-      else if (joint_names[i] == "thj5")
-        f.data = 0.30 - msg->data * 7;
+      if (joint_names[i] == "ffj3"){
+        arm_joint_trajectory.joint_names.emplace_back("rh_FFJ3");
+        arm_joint_trajectory.points.back().positions.emplace_back(1.47 - msg->data * 10);
+}
+      else if (joint_names[i] == "mfj3"){
+        arm_joint_trajectory.joint_names.emplace_back("rh_MFJ3");
+        arm_joint_trajectory.points.back().positions.emplace_back(1.57 - msg->data * 10);
+}
+      else if (joint_names[i] == "thj4"){
+                arm_joint_trajectory.joint_names.emplace_back("rh_THJ4");
+        arm_joint_trajectory.points.back().positions.emplace_back(1.20);
+       }
+      else if (joint_names[i] == "thj5"){
+        arm_joint_trajectory.joint_names.emplace_back("rh_THJ5");
+        arm_joint_trajectory.points.back().positions.emplace_back(0.30 - msg->data * 7);
+}
       else
-        f.data = 0.0;
-
-      publisher[i].publish(f);
+        continue;
+      arm_joint_trajectory.points.back().time_from_start =
+                                  ros::Duration(1.0 / 30);
+      pub.publish(arm_joint_trajectory);
     }
   }
 };
